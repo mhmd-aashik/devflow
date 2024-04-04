2d04a2abdc2ca05c479fa912c61aefcb10a0193d
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-04 13:50:03 +0530

    Home Filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
 
@@ -24,8 +27,13 @@ const Home = () => {
           placeholder="Search for Questions"
           otherClasses="flex-1"
         />
-        Filters
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+      <HomeFilters/>
     </>
   );
 };
--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -43,8 +43,8 @@ const LeftSideBar = () => {
           <Link href="/sign-in">
             <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
               <Image
-                src="/assets/icons/sign-up.svg"
-                alt="sign up"
+                src="/assets/icons/account.svg"
+                alt="login"
                 width={20}
                 height={20}
                 className="invert-colors lg:hidden"
