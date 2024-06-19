a7ba4b10e102675f788dc89f4e37c694c5b8725f
mhmd-aashik <aashikdevelop@gmail.com>
2024-06-19 06:50:17 +0530

    Create a Loading state for the Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -18,8 +18,6 @@ export default async function Home({ searchParams }: SearchParamsProps) {
     page: searchParams.page ? +searchParams.page : 1,
   });
 
-  // fetch recommanded questions
-
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
