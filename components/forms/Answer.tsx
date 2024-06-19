b14c54b16d055a86845c9e2f25a4f61e94d03ed1
mhmd-aashik <aashikdevelop@gmail.com>
2024-06-19 06:09:43 +0530

    Display the AI results on the UI

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+
 import {
   Form,
   FormControl,
@@ -8,31 +7,30 @@ import {
   FormItem,
   FormMessage,
 } from "../ui/form";
-import { AnswerSchema } from "@/lib/validation";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
+import { useRef, useState } from "react";
 import { useTheme } from "@/context/ThemeProvider";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import { createAnswer } from "@/lib/actions/answer.action";
 import { usePathname } from "next/navigation";
+import { AnswerSchema } from "@/lib/validation";
 
 interface Props {
-  userId: string;
-  questionId: string;
   question: string;
+  questionId: string;
+  userId: string;
 }
 
-const Answer = ({ userId, questionId, question }: Props) => {
+const Answer = ({ question, questionId, userId }: Props) => {
   const pathname = usePathname();
-
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isSubmittingAI, setIsSubmittingAI] = useState(false);
+  const [isSubmittingAI, setSetIsSubmittingAI] = useState(false);
   const { mode } = useTheme();
-
   const editorRef = useRef(null);
-
   const form = useForm<z.infer<typeof AnswerSchema>>({
     resolver: zodResolver(AnswerSchema),
     defaultValues: {
@@ -42,6 +40,7 @@ const Answer = ({ userId, questionId, question }: Props) => {
 
   const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
     setIsSubmitting(true);
+
     try {
       await createAnswer({
         content: values.answer,
@@ -67,7 +66,7 @@ const Answer = ({ userId, questionId, question }: Props) => {
   const generateAIAnswer = async () => {
     if (!userId) return;
 
-    setIsSubmittingAI(true);
+    setSetIsSubmittingAI(true);
 
     try {
       const response = await fetch(
@@ -79,11 +78,20 @@ const Answer = ({ userId, questionId, question }: Props) => {
       );
 
       const aiAnswer = await response.json();
-      alert(aiAnswer.reply);
+      // Convert plain text to HTML format
+
+      const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedAnswer);
+      }
+
+      // Toast...
     } catch (error) {
       console.log(error);
     } finally {
-      setIsSubmittingAI(false);
+      setSetIsSubmittingAI(false);
     }
   };
 
@@ -93,20 +101,28 @@ const Answer = ({ userId, questionId, question }: Props) => {
         <h4 className="paragraph-semibold text-dark400_light800">
           Write your answer here
         </h4>
+
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           onClick={generateAIAnswer}
         >
-          <Image
-            src="/assets/icons/stars.svg"
-            alt="star"
-            width={12}
-            height={12}
-            className="object-contain"
-          />
-          Generate AI Answer
+          {isSubmittingAI ? (
+            <>Generating...</>
+          ) : (
+            <>
+              <Image
+                src="/assets/icons/stars.svg"
+                alt="star"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate AI Answer
+            </>
+          )}
         </Button>
       </div>
+
       <Form {...form}>
         <form
           className="mt-6 flex w-full flex-col gap-10"
@@ -118,13 +134,12 @@ const Answer = ({ userId, questionId, question }: Props) => {
             render={({ field }) => (
               <FormItem className="flex w-full flex-col gap-3">
                 <FormControl className="mt-3.5">
-                  {/* TODO : Add editor components */}
                   <Editor
                     apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                    onInit={(evt, editor) =>
+                    onInit={(evt, editor) => {
                       // @ts-ignore
-                      (editorRef.current = editor)
-                    }
+                      editorRef.current = editor;
+                    }}
                     onBlur={field.onBlur}
                     onEditorChange={(content) => field.onChange(content)}
                     init={{
@@ -148,11 +163,11 @@ const Answer = ({ userId, questionId, question }: Props) => {
                         "table",
                       ],
                       toolbar:
-                        "undo redo | blocks | " +
-                        "codesample | bold italic forecolor | alignleft aligncenter " +
-                        "alignright alignjustify | bullist numlist ",
+                        "undo redo | " +
+                        "codesample | bold italic forecolor | alignleft aligncenter |" +
+                        "alignright alignjustify | bullist numlist",
                       content_style:
-                        "body { font-family:Inter, font-size:16px }",
+                        "body { font-family:Inter; font-size:16px }",
                       skin: mode === "dark" ? "oxide-dark" : "oxide",
                       content_css: mode === "dark" ? "dark" : "light",
                     }}
